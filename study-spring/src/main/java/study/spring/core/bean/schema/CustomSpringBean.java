480a9d2706c355ffd960e90ec05ec38088689349
RevelationColleciton <310002170@qq.com>
2020-07-14 16:45:12 +0800

    spring xsd code

--- study-spring/src/main/java/study/spring/core/bean/schema/CustomSpringBean.java
+++ study-spring/src/main/java/study/spring/core/bean/schema/CustomSpringBean.java
@@ -7,5 +7,15 @@ public class CustomSpringBean {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("custom-xsd-spring.xml");
         Woman woman = (Woman) context.getBean("woman");
         System.out.println(woman);
+        /*
+            https://www.cnblogs.com/jifeng/archive/2011/09/14/2176599.html
+            study.spring.core.bean.schema.Woman
+            study.spring.core.bean.schema.MyNamespaceHandler
+            study.spring.core.bean.schema.WomanBeanDefinitionParser
+            src/main/resources/META-INF/spring.handlers
+            src/main/resources/META-INF/spring.schemas
+            src/main/resources/META-INF/woman.xsd
+            src/main/resources/custom-xsd-spring.xml
+         */
     }
 }
